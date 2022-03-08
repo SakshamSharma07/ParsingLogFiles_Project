27ec4fb0802043bfef93f03383f92dc9f7512ab0
Saksham Sharma <89659787+SakshamSharma07@users.noreply.github.com>
2022-03-08 16:33:26 +0530

    Add files via upload
    this is a python program to parse log file data and save them in a formatted way in a text file.
    #cybeHawkz_project #cgcWorkshop

---
+++ Project1.py
@@ -0,0 +1,15 @@
+from dataclasses import replace
+with open("website.txt",'r') as f:
+    lines=f.readlines()
+    with open("output.txt","w+") as of:
+        of.write(" IP\t\t\tDate Time\t\t\t\tRequest\t\tProtocol\tStatus Code\tPacket Size\n")
+        for i in lines:
+            i=i.replace('[','')
+            i=i.replace(']','')
+            i=i.replace('"','')
+            line =i.split()
+            for i in range(len(line[0]),16):
+                of.write(" ")
+                of.write(f"{line[0]}\t\t{line[3]+line[4]}\t\t{line[5]}\t\t{line[7]}\t{line[8]}\t\t{line[9]}\n")
+    of.close()
+    f.close()
